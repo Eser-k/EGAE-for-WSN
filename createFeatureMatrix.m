2145a1030e3e77140a26ff6ab1b6239c357405a4
Eser-k <eser.kayali@adesso.de>
2025-06-09 20:51:56 +0200

    Clustering with DBSCAN

--- createFeatureMatrix.m
+++ createFeatureMatrix.m
@@ -18,11 +18,8 @@ function X = createFeatureMatrix(Sensors, Model)
     % number of sensor nodes
     n = Model.n;        
     
-    % radius for neighbor connectivity
-    adjacencyDistance = 30;
-    
     % preallocate feature matrix (n rows, 5 columns)
-    X = zeros(n, 5);    
+    X = zeros(n, 2);    
     
     for i = 1:n
         % -----------------------------------------------------------------
@@ -30,39 +27,6 @@ function X = createFeatureMatrix(Sensors, Model)
         % -----------------------------------------------------------------
         X(i,1) = Sensors(i).xd; 
         X(i,2) = Sensors(i).yd;
-    
-        % -----------------------------------------------------------------
-        % 3) Distance to sink 
-        % ----------------------------------------------------------------- 
-        X(i,3) = Sensors(i).dis2sink;
-        
-        % -----------------------------------------------------------------
-        % 4–5) Local neighborhood statistics:
-        %       - degree (number of neighbors within adjacencyDistance)
-        %       - average neighbor distance
-        % -----------------------------------------------------------------
-        neighborCount = 0;
-        totalNeighborDistance = 0;
-    
-        for j = 1:n
-            if i ~= j
-                d = sqrt((Sensors(i).xd - Sensors(j).xd)^2 + (Sensors(i).yd - Sensors(j).yd)^2);
-                if d <= adjacencyDistance
-                    neighborCount = neighborCount + 1;
-                    totalNeighborDistance = totalNeighborDistance + d;
-                end
-            end
-        end
-    
-        X(i,4) = neighborCount;
-    
-        if neighborCount > 0
-            X(i,5) = (totalNeighborDistance / neighborCount);
-        else
-            % if no neighbors, use adjacencyDistance as a placeholder
-            X(i,5) = adjacencyDistance; 
-        end
-        
     end
     
 end
