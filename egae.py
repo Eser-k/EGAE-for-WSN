ce1c27415e0229464d6558abbb2dbaff967e4bd5
Eser-k <eser.kayali@adesso.de>
2025-06-16 20:54:37 +0200

    Changed hidden dims to lower dimensions

--- egae.py
+++ egae.py
@@ -24,7 +24,7 @@ class EGAE(tf.keras.Model):
 
         # Layer-Architecture
         if hidden_dims is None:
-            hidden_dims = [256, 128]
+            hidden_dims = [16, 8]
         self.hidden_dims = hidden_dims
 
         # Activation Function
