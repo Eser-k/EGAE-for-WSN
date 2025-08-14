fc6f33107635d32df00d19ce7a729bda7d032259
Eser-k <eser.kayali@adesso.de>
2025-08-14 20:16:35 +0200

    Improved the bug that data goes to dead Clusterhead.

--- simulation.m
+++ simulation.m
@@ -287,7 +287,7 @@ for r=1:1:Model.rmax
     end
 
     % Briefly pause so that each round’s links are visible before updating
-    pause(0.1);
+    pause(0.05);
     
 %%%%%%%%%%%%%%%%%%%%% steady-state phase %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     % In the steady-state phase, each alive sensor sends its data 
@@ -315,7 +315,7 @@ for r=1:1:Model.rmax
     [i,j] = find(triu(isfinite(W),1));
     w = W(sub2ind([m m], i, j));
     
-    MG = graph(i, j, w);  % MATLAB Graphobject
+    MG = graph(i, j, w, m);  % MATLAB Graphobject
     
     s = G.sinkIndex;
     
@@ -337,7 +337,7 @@ for r=1:1:Model.rmax
     'DataAspectRatio', [1 1 1], ...      
     'PlotBoxAspectRatioMode', 'auto');
     
-    pause(0.2);
+    pause(0.05);
     
 
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -382,12 +382,20 @@ for r=1:1:Model.rmax
     
             recvId = G.nodeIds(p);   % Sensor-ID des Empfängers (Parent)
 
+            if Sensors(recvId).E <= 0 
+                recvId = n+1;
+                fprintf('Receiver has no Energy, send directly to sink \n');
+                Sensors = SendReceivePackets(Sensors, Model, senderId, 'Data', recvId);
+                break; 
+            end 
+
             fprintf('senderId = %d\n', senderId);
             fprintf('receiverId = %d\n', recvId);
 
             Sensors = SendReceivePackets(Sensors, Model, senderId, 'Data', recvId);
     
             if Sensors(recvId).E <= 0, break; end 
+            
             u = p;           
             senderId = recvId;
         end
