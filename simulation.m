e7fa5bd873f6e4e7b6bf1871da66eaf445885aaf
Eser-k <eser.kayali@adesso.de>
2025-08-14 16:03:06 +0200

    Implemented Multi-Hop for Hierarchical Clustering.

--- simulation.m
+++ simulation.m
@@ -79,10 +79,11 @@ SumEnergyAllSensor(1) = initEnergy;
 alive = n;
 AliveSensors(1)= n;
 
-%%%%%%%%%%%%%%%%%% cluster with kMeans  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-positons = createFeatureMatrix(Sensors,Model);
+%%%%%%%%%%%%%%%%%% cluster with hierarchical clustering  %%%%%%%%%%%%%%%%%%
+positions = createFeatureMatrix(Sensors,Model);
 kmax = int32(n/5);
-inertias = computeInertia(positons, kmax);
+
+inertias = computeInertia(positions, kmax);
 
 % Visualize the inertia values
 figure('Name','Elbow','NumberTitle','off');
@@ -94,11 +95,31 @@ grid on;
 
 % Determine the optimal number of clusters by 
 % finding the “elbow” in the inertia curve
-k_opt = findElbow(inertias);
-fprintf('Optimal number of clusters (Elbow): %d\n', k_opt);
+num_clusters = findElbow(inertias);
+fprintf('Optimal number of clusters (Elbow): %d\n', num_clusters);
 
-num_clusters = k_opt;
-[cluster_labels, centroids] = kmeans(positons, k_opt, 'Replicates',5, 'MaxIter',300);
+distances = pdist(positions);
+Z = linkage(distances,"ward");
+
+cluster_labels = cluster(Z, 'maxclust', num_clusters); 
+
+figure('Name','Dendrogram','NumberTitle','off');
+h = dendrogram(Z,0, ClusterIndices=cluster_labels, ...
+    ShowMarkers=true,ShowCut=true);
+fs = 15;                         
+
+% Achsenbeschriftungen und Tick-Labels anpassen
+set(gca,'FontSize',fs)
+
+% Alle Text-Objekte (u. a. Knotentexte der Cluster) anpassen
+set(findall(gcf,'Type','text'),'FontSize',fs)
+
+% Optional: Linienbreite etwas erhöhen, damit der Baum im Druck klarer wirkt
+set(h,'LineWidth',1.4)
+
+xlabel('Sensor');
+ylabel('Abstand');
+title('Hierarchisches Clustering (ward)');
 
 % Generate a palette of distinct colors (one per cluster) 
 cmap = jet(num_clusters);
